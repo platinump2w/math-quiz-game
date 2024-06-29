7a51b3abc9f3b2ce574d4a36b3f47c19ac5dd448
Guilherme Felipe <guilherme.fa616@gmail.com>
2024-06-29 10:19:56 -0300

    Updates

---
+++ precache-manifest.4424b19babf98a04dd72724871a7572a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73355367e31b4e25ce35",
+    "url": "/math-quiz-game/static/css/main.6b2be876.chunk.css"
+  },
+  {
+    "revision": "73355367e31b4e25ce35",
+    "url": "/math-quiz-game/static/js/main.06b15302.chunk.js"
+  },
+  {
+    "revision": "3614bf9e069dfc3c187d",
+    "url": "/math-quiz-game/static/js/runtime~main.d5ceb3ba.js"
+  },
+  {
+    "revision": "b7b404d108649af495dc",
+    "url": "/math-quiz-game/static/js/2.b49f59df.chunk.js"
+  },
+  {
+    "revision": "f9b3d76d21086326ccabf0a0d2561af1",
+    "url": "/math-quiz-game/index.html"
+  }
+];
